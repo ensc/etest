aaee1f8b83d4412cc60538ff388ed9053bbd6469
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2024-08-31 12:39:32 +0200

    lib: make ResourceId a Cow<> based enum

--- src/resource/id.rs
+++ src/resource/id.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 /// A resource which can be "used" or "consumed"
 ///
 /// To be used with the `uses` and `consumes` parameters of `#[etest]`.  For type
@@ -36,24 +38,32 @@
 /// #[etest(consumes=[Output])]
 /// fn test() {}
 /// ```
-
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
-pub struct ResourceId {
-    id:		String,
+pub enum ResourceIdImpl<'a> {
+    /// Normal resource
+    Id(Cow<'a, str>),
 }
 
-impl ResourceId {
-    pub fn new(id: &str) -> Self {
-        Self {
-            id:	id.to_string()
-        }
+pub type ResourceId = ResourceIdImpl<'static>;
+
+impl <'a> ResourceIdImpl<'a> {
+    pub const fn new(id: &'a str) -> Self {
+        Self::Id(Cow::Borrowed(id))
+    }
+
+    pub fn from_string(s: String) -> Self {
+        Self::Id(Cow::Owned(s))
     }
 }
 
-impl From<&str> for ResourceId {
-    fn from(val: &str) -> Self {
-        ResourceId {
-            id:	val.to_string()
-        }
+impl <'a> From<&'a str> for ResourceIdImpl<'a> {
+    fn from(val: &'a str) -> Self {
+        Self::new(val)
+    }
+}
+
+impl From<String> for ResourceIdImpl<'_> {
+    fn from(val: String) -> Self {
+        Self::from_string(val)
     }
 }
